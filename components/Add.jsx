2cfc55d2de9470ba3b9a53ccb49ed0643743f955
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-02 22:57:48 +0530

    chore: setup networking for GET and POST

--- components/Add.jsx
+++ components/Add.jsx
@@ -1,30 +1,48 @@
 import React from "react";
 import { useState } from "react";
 import "../src/App.css";
+import axios from "axios";
 
 function Add() {
   const [name, setName] = useState("");
   const [content, setContent] = useState("");
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/messages/add",
+        {
+          name: name,
+          content: content,
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="compose"> 
-      <input
-        className=""
-        placeholder="Enter Name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <textarea
-        className=""
-        placeholder="Enter Message"
-        type="text"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <button className="" type="submit">
-        Send
-      </button>
+    <div className="compose">
+      <form onSubmit={handleSubmit}>
+        <input
+          className=""
+          placeholder="Enter Name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <textarea
+          className=""
+          placeholder="Enter Message"
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  // const [name, setName] = useState("");
+  // const [content, setContent] = useState("");
+  const [wholeData, setWholeData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/messages")
+      .then((res) => {
+        console.log(res);
+        setWholeData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return <div>{wholeData.map((item) => <p>{item.name}</p>)}</div>;
 }
 
-export default Home
+export default Home;
