49cf01320fd4eabecb784effb21f06b39750b41e
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-02 15:42:22 +0530

    chore: created components and packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="">
-    
-  </div>;
+  return <BrowserRouter>
+  <Navbar />
+  <Routes>
+    <Route path="/" element={<Home  />} />
+    <Route path="/add" element={<Add />} />
+  </Routes>
+</BrowserRouter>;
 }
 
 export default App;
