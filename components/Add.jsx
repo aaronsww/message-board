ed68801c782e5a94fccab929f3144d9ef103a991
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-02 16:16:24 +0530

    chore: input fields for sending message

--- components/Add.jsx
+++ components/Add.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import "../src/App.css";
 
 function Add() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
   return (
-    <div>
-         
+    <div className="compose"> 
+      <input
+        className=""
+        placeholder="Enter Name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <textarea
+        className=""
+        placeholder="Enter Message"
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button className="" type="submit">
+        Send
+      </button>
     </div>
-  )
+  );
 }
 
-export default Add
+export default Add;
--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "../components/Home";
+import Add from "../components/Add";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <BrowserRouter>
-  <Navbar />
-  <Routes>
-    <Route path="/" element={<Home  />} />
-    <Route path="/add" element={<Add />} />
-  </Routes>
-</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
